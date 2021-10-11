625db2db8fa5d9641df56c3de969aa3686c81b1e
ABlued <53801395+ABlued@users.noreply.github.com>
2021-10-11 18:24:43 +0900

    이벤트 위임 처리를 사용하여 이벤트가 잘 발생하지 않았던 버그 수정

--- src/main.js
+++ src/main.js
@@ -1,105 +1,111 @@
 //JSON 파일에 있는 데이터를 받아오는 함수
-function loadItems(){
-    return fetch('data/data.json')     //이 경로안에 있는 데이터를 받아온다
-    .then(response => response.json())        //패치가 완료되면 response라는 객체를 전달받게 되고 json으로 변환.
-    .then(json => json.items);          //json안에 있는 items를 반환받는다.
+const container = document.querySelector(".items");
+function loadItems() {
+  return fetch("data/data.json") //이 경로안에 있는 데이터를 받아온다
+    .then((response) => response.json()) //패치가 완료되면 response라는 객체를 전달받게 되고 json으로 변환.
+    .then((json) => json.items); //json안에 있는 items를 반환받는다.
 }
 //JSON에서 받아온 데이터를 출력하는 함수
-function displayItems(itmes){
-    const container = document.querySelector('.items');
-    // container.innerHTML = itmes.map(item => createHTMLString(item)).join('');       //받아온 것들을 문자열로 만들고 join으로 나눈다
-    while(container.hasChildNodes()){       // 이미 화면에 나타난 상품이 있을 경우
-        container.removeChild(container.firstChild);        // 첫번째 자식을 지워 삭제한다
-    }
-    itmes.map(item => createHTMLElement(item));     // 모두 삭제한 후 조건에 맞는 상품들을 다시 표시한다.
+function displayItems(items) {
+  const container = document.querySelector(".items");
+  // container.innerHTML = itmes.map(item => createHTMLString(item)).join('');       //받아온 것들을 문자열로 만들고 join으로 나눈다
+  while (container.hasChildNodes()) {
+    // 이미 화면에 나타난 상품이 있을 경우
+    container.removeChild(container.firstChild); // 첫번째 자식을 지워 삭제한다
+  }
+  items.map((item) => createHTMLElement(item)); // 모두 삭제한 후 조건에 맞는 상품들을 다시 표시한다.
+  container.addEventListener("mouseover", displayDetail);
+  container.addEventListener("mouseout", hiddenDetail);
+}
+function displayDetail(event) {
+  const li = event.target.closest("li");
+  if (!li) return;
+  if (!container.contains(li)) return;
+  const target = li.children[2];
+  target.style.display = "block";
+}
+function hiddenDetail(event) {
+  const li = event.target.closest("li");
+  if (!li) return;
+  if (!container.contains(li)) return;
+  const target = li.children[2];
+  target.style.display = "none";
 }
 //JSON에서 받아온 데이터를 문자열로 변환하는 함수
 // function createHTMLString(item){
-    //     return `
-    //     <li onclick="displayDetail()" onmouseout="hiddenDetail()" class="item">
-    //     <img src="${item.image}" alt="${item.type}" class="item__thumbnail"/>
-    //     <span class="item__description">${item.gender}, ${item.size}</span>
-    //     <div class="item__detail" style="display: none;">
-    //         <span class="item__detail__element">길이 : ${item.length} 소재 : ${item.Material} 별점: ${item.TotalScore}</span>
-    //         <span class="item__detail__element">설명 : ${item.comment}</span>
-    //     </div>
-    //   </li>
-    //   `;// onmouseenter="displayDetail()" onmouseout="hiddenDetail()"
+//     return `
+//     <li onclick="displayDetail()" onmouseout="hiddenDetail()" class="item">
+//     <img src="${item.image}" alt="${item.type}" class="item__thumbnail"/>
+//     <span class="item__description">${item.gender}, ${item.size}</span>
+//     <div class="item__detail" style="display: none;">
+//         <span class="item__detail__element">길이 : ${item.length} 소재 : ${item.Material} 별점: ${item.TotalScore}</span>
+//         <span class="item__detail__element">설명 : ${item.comment}</span>
+//     </div>
+//   </li>
+//   `;// onmouseenter="displayDetail()" onmouseout="hiddenDetail()"
 // }
 
 //JSON에서 받아온 데이터를 기반으로 HTMLElement를 생성하는 함수
-function createHTMLElement(item){
-  const parentElement = document.querySelector('.items');
+function createHTMLElement(item) {
+  const parentElement = document.querySelector(".items");
   const li = document.createElement("li");
-  li.addEventListener("click",displayDetail);
-  li.addEventListener("mouseout",hiddenDetail);
-  li.setAttribute("class","item");
+  // li.addEventListener("mouseover", displayDetail);
+  // li.addEventListener("mouseout", hiddenDetail);
+  li.setAttribute("class", "item");
 
   const img = document.createElement("img");
   img.src = `${item.image}`;
   img.alt = `${item.type}`;
-  img.setAttribute("class","item__thumbnail");
+  img.setAttribute("class", "item__thumbnail");
 
   const span = document.createElement("span");
   span.setAttribute("class", "item__description");
   span.innerText = `${item.gender}` + ", " + `${item.size}`;
 
   const div = document.createElement("div");
-  div.setAttribute("class","item__detail");
+  div.setAttribute("class", "item__detail");
   div.style.display = "none";
 
   const spanInDiv1 = document.createElement("span");
-  spanInDiv1.setAttribute("class","item__detail__element")
-  spanInDiv1.innerText = "길이 : " + `${item.length}` + " 소재 : " + `${item.Material}` + " 별점 : " + `${item.TotalScore}`;
+  spanInDiv1.setAttribute("class", "item__detail__element");
+  spanInDiv1.innerText =
+    "길이 : " +
+    `${item.length}` +
+    " 소재 : " +
+    `${item.Material}` +
+    " 별점 : " +
+    `${item.TotalScore}`;
 
   div.appendChild(spanInDiv1);
-  
+
   li.appendChild(img);
   li.appendChild(span);
   li.appendChild(div);
   parentElement.appendChild(li);
 }
 
-// 해당 상품을 클릭 시 상세정보를 보여주는 함수
-function displayDetail(event){
-    const target = event.target.children[2];
-    console.log(target);
-    target.style.display = "block";
-    // const item__detail = document.querySelector('.item__detail');
-    // item__detail.style.display = "block";
+function setEventListeners(items) {
+  const logo = document.querySelector(".logo");
+  const buttons = document.querySelector(".buttons");
+  logo.addEventListener("click", () => displayItems(items));
+  buttons.addEventListener("click", (event) => onButtonClick(event, items));
 }
 
-// 해당 상품에서 마우스포인터가 빠져나올시 상세정보를 감추는 함수
-function hiddenDetail(event){
-    const target = event.target.children[2];
-    console.log(target);
-    target.style.display = "none";
-    // const item__detail = document.querySelector('.item__detail');
-    // item__detail.style.display = "none";
-}
+function onButtonClick(event, items) {
+  const dataSet = event.target.dataset;
+  const key = dataSet.key;
+  const value = dataSet.value;
 
-function setEventListeners(items){
-    const logo = document.querySelector('.logo');
-    const buttons = document.querySelector('.buttons');
-    logo.addEventListener('click', () => displayItems(items));
-    buttons.addEventListener('click',event => onButtonClick(event, items));
-}
+  if (key == null || value == null) {
+    return;
+  }
 
-function onButtonClick(event, items){
-    const dataSet = event.target.dataset;
-    const key = dataSet.key;
-    const value = dataSet.value;
-    
-    if(key == null || value == null){
-        return;
-    }
-
-    displayItems(items.filter(item => item[key] === value));
+  displayItems(items.filter((item) => item[key] === value));
 }
 
 loadItems()
-.then(items => {
+  .then((items) => {
     displayItems(items);
-    setEventListeners(items)
-})
-.catch(console.log);
+    setEventListeners(items);
+  })
+  .catch(console.log);
