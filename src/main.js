8398bd6bb33ef99823bfc34b2437288f812f2a60
ABlued <skwodhkswkd@naver.com>
2023-01-14 23:34:29 +0900

    prettier 적용

--- src/main.js
+++ src/main.js
@@ -1,79 +1,64 @@
 //JSON 파일에 있는 데이터를 받아오는 함수
-const container = document.querySelector(".items");
+const container = document.querySelector('.items');
 function loadItems() {
-  return fetch("data/data.json") //이 경로안에 있는 데이터를 받아온다
+  return fetch('data/data.json') //이 경로안에 있는 데이터를 받아온다
     .then((response) => response.json()) //패치가 완료되면 response라는 객체를 전달받게 되고 json으로 변환.
     .then((json) => json.items); //json안에 있는 items를 반환받는다.
 }
 //JSON에서 받아온 데이터를 출력하는 함수
 function displayItems(items) {
-  const container = document.querySelector(".items");
+  const container = document.querySelector('.items');
   // container.innerHTML = itmes.map(item => createHTMLString(item)).join('');       //받아온 것들을 문자열로 만들고 join으로 나눈다
   while (container.hasChildNodes()) {
     // 이미 화면에 나타난 상품이 있을 경우
     container.removeChild(container.firstChild); // 첫번째 자식을 지워 삭제한다
   }
   items.map((item) => createHTMLElement(item)); // 모두 삭제한 후 조건에 맞는 상품들을 다시 표시한다.
-  container.addEventListener("mouseover", displayDetail);
-  container.addEventListener("mouseout", hiddenDetail);
+  container.addEventListener('mouseover', displayDetail);
+  container.addEventListener('mouseout', hiddenDetail);
 }
 function displayDetail(event) {
-  const li = event.target.closest("li");
+  const li = event.target.closest('li');
   if (!li) return;
   if (!container.contains(li)) return;
   const target = li.children[2];
-  target.style.display = "block";
+  target.style.display = 'block';
 }
 function hiddenDetail(event) {
-  const li = event.target.closest("li");
+  const li = event.target.closest('li');
   if (!li) return;
   if (!container.contains(li)) return;
   const target = li.children[2];
-  target.style.display = "none";
+  target.style.display = 'none';
 }
-//JSON에서 받아온 데이터를 문자열로 변환하는 함수
-// function createHTMLString(item){
-//     return `
-//     <li onclick="displayDetail()" onmouseout="hiddenDetail()" class="item">
-//     <img src="${item.image}" alt="${item.type}" class="item__thumbnail"/>
-//     <span class="item__description">${item.gender}, ${item.size}</span>
-//     <div class="item__detail" style="display: none;">
-//         <span class="item__detail__element">길이 : ${item.length} 소재 : ${item.Material} 별점: ${item.TotalScore}</span>
-//         <span class="item__detail__element">설명 : ${item.comment}</span>
-//     </div>
-//   </li>
-//   `;// onmouseenter="displayDetail()" onmouseout="hiddenDetail()"
-// }
 
 //JSON에서 받아온 데이터를 기반으로 HTMLElement를 생성하는 함수
 function createHTMLElement(item) {
-  const parentElement = document.querySelector(".items");
-  const li = document.createElement("li");
-  // li.addEventListener("mouseover", displayDetail);
-  // li.addEventListener("mouseout", hiddenDetail);
-  li.setAttribute("class", "item");
+  const parentElement = document.querySelector('.items');
+  const li = document.createElement('li');
+  li.setAttribute('class', 'item');
 
-  const img = document.createElement("img");
+  const img = document.createElement('img');
   img.src = `${item.image}`;
   img.alt = `${item.type}`;
-  img.setAttribute("class", "item__thumbnail");
+  img.setAttribute('class', 'item__thumbnail');
 
-  const span = document.createElement("span");
-  span.setAttribute("class", "item__description");
-  span.innerText = `${item.gender}` + ", " + `${item.size}`;
+  const span = document.createElement('span');
+  span.setAttribute('class', 'item__description');
+  span.innerText = `${item.gender}` + ', ' + `${item.size}`;
 
-  const div = document.createElement("div");
-  div.setAttribute("class", "item__detail");
-  div.style.display = "none";
+  const div = document.createElement('div');
+  div.setAttribute('class', 'item__detail');
+  div.style.display = 'none';
 
-  const spanInDiv1 = document.createElement("span");
-  spanInDiv1.setAttribute("class", "item__detail__element");
+  const spanInDiv1 = document.createElement('span');
+  spanInDiv1.setAttribute('class', 'item__detail__element');
   spanInDiv1.innerText =
-    "길이 : " +
+    '길이 : ' +
     `${item.length}` +
-    " 소재 : " +
+    ' 소재 : ' +
     `${item.Material}` +
-    " 별점 : " +
+    ' 별점 : ' +
     `${item.TotalScore}`;
 
   div.appendChild(spanInDiv1);
@@ -85,10 +70,10 @@ function createHTMLElement(item) {
 }
 
 function setEventListeners(items) {
-  const logo = document.querySelector(".logo");
-  const buttons = document.querySelector(".buttons");
-  logo.addEventListener("click", () => displayItems(items));
-  buttons.addEventListener("click", (event) => onButtonClick(event, items));
+  const logo = document.querySelector('.logo');
+  const buttons = document.querySelector('.buttons');
+  logo.addEventListener('click', () => displayItems(items));
+  buttons.addEventListener('click', (event) => onButtonClick(event, items));
 }
 
 function onButtonClick(event, items) {
